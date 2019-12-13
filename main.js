75afb1c01e5a5c97ed2d44485f204fae117e25f0
sarraHa <sara.benloulou@gmail.com>
2019-12-13 16:05:47 +0100

    fist commit*

---
+++ main.js
@@ -0,0 +1,15 @@
+//Select element function
+
+/*new for me*/ 
+const selectElement = function(element){
+    return document.querySelector(element);
+};
+
+
+let menuToggler = selectElement('.menu-toggle');
+let body = selectElement('body');
+
+/*new for me*/ 
+menuToggler.addEventListener('click', function(){
+    body.classList.toggle('open');
+});
